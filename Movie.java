1eafb5389ffef33b2b5a473d804f268ec092ac6a
mirai <xiaotututuer@gmail.com>
2018-08-03 19:17:57 +0800

    MovieBean
    添加了movie的bean

---
+++ Movie.java
@@ -0,0 +1,216 @@
+package com.sotto.bean;
+
+public class Movie {
+
+  private long id;
+  private String moviename;
+  private String englishname;
+  private long dataId;
+  private String director;
+  private String star;
+  private String type1;
+  private String type2;
+  private String type3;
+  private String picture;
+  private String place;
+  private double score;
+  private long years;
+  private String introduction;
+  private long timelong;
+  private String link;
+  private long starId;
+  private long directorId;
+  private long greatCount;
+  private long collectCount;
+  private long clickCount;
+
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+
+  public String getMoviename() {
+    return moviename;
+  }
+
+  public void setMoviename(String moviename) {
+    this.moviename = moviename;
+  }
+
+
+  public String getEnglishname() {
+    return englishname;
+  }
+
+  public void setEnglishname(String englishname) {
+    this.englishname = englishname;
+  }
+
+
+  public long getDataId() {
+    return dataId;
+  }
+
+  public void setDataId(long dataId) {
+    this.dataId = dataId;
+  }
+
+
+  public String getDirector() {
+    return director;
+  }
+
+  public void setDirector(String director) {
+    this.director = director;
+  }
+
+
+  public String getStar() {
+    return star;
+  }
+
+  public void setStar(String star) {
+    this.star = star;
+  }
+
+
+  public String getType1() {
+    return type1;
+  }
+
+  public void setType1(String type1) {
+    this.type1 = type1;
+  }
+
+
+  public String getType2() {
+    return type2;
+  }
+
+  public void setType2(String type2) {
+    this.type2 = type2;
+  }
+
+
+  public String getType3() {
+    return type3;
+  }
+
+  public void setType3(String type3) {
+    this.type3 = type3;
+  }
+
+
+  public String getPicture() {
+    return picture;
+  }
+
+  public void setPicture(String picture) {
+    this.picture = picture;
+  }
+
+
+  public String getPlace() {
+    return place;
+  }
+
+  public void setPlace(String place) {
+    this.place = place;
+  }
+
+
+  public double getScore() {
+    return score;
+  }
+
+  public void setScore(double score) {
+    this.score = score;
+  }
+
+
+  public long getYears() {
+    return years;
+  }
+
+  public void setYears(long years) {
+    this.years = years;
+  }
+
+
+  public String getIntroduction() {
+    return introduction;
+  }
+
+  public void setIntroduction(String introduction) {
+    this.introduction = introduction;
+  }
+
+
+  public long getTimelong() {
+    return timelong;
+  }
+
+  public void setTimelong(long timelong) {
+    this.timelong = timelong;
+  }
+
+
+  public String getLink() {
+    return link;
+  }
+
+  public void setLink(String link) {
+    this.link = link;
+  }
+
+
+  public long getStarId() {
+    return starId;
+  }
+
+  public void setStarId(long starId) {
+    this.starId = starId;
+  }
+
+
+  public long getDirectorId() {
+    return directorId;
+  }
+
+  public void setDirectorId(long directorId) {
+    this.directorId = directorId;
+  }
+
+
+  public long getGreatCount() {
+    return greatCount;
+  }
+
+  public void setGreatCount(long greatCount) {
+    this.greatCount = greatCount;
+  }
+
+
+  public long getCollectCount() {
+    return collectCount;
+  }
+
+  public void setCollectCount(long collectCount) {
+    this.collectCount = collectCount;
+  }
+
+
+  public long getClickCount() {
+    return clickCount;
+  }
+
+  public void setClickCount(long clickCount) {
+    this.clickCount = clickCount;
+  }
+
+}
